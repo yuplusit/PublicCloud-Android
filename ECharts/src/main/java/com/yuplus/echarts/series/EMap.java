a156b1539fe9b4a38ac2e1cf7862e555c2f4fc93
zlzsam <zlzsam@hotmail.com>
2017-05-22 19:32:47 +0800

    ECharts包装层封装

---
+++ ECharts/src/main/java/com/yuplus/echarts/series/EMap.java
@@ -0,0 +1,15 @@
+package com.yuplus.echarts.series;
+
+/**
+ * 和Map完全相同，只是为了避免和java.util.Map重名
+ *
+ * @author liuzh
+ */
+public class EMap extends Map {
+    public EMap() {
+    }
+
+    public EMap(String name) {
+        super(name);
+    }
+}
